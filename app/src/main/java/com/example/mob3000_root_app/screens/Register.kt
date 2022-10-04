6522eb903270b7521f43262104b057edbb02de55
johlil9 <243333@usn.no>
2022-10-04 14:36:19 +0200

    Update on Login and Register. Button functions. Added card background.

--- app/src/main/java/com/example/mob3000_root_app/screens/Register.kt
+++ app/src/main/java/com/example/mob3000_root_app/screens/Register.kt
@@ -1,13 +1,16 @@
 package com.example.mob3000_root_app.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -15,6 +18,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+//import androidx.compose.foundation.layout.BoxScopeInstance.align
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,93 +29,133 @@ fun Register(navController: NavHostController) {
     var passord       by remember{ mutableStateOf(TextFieldValue("")) }
     var gjentaPassord by remember{ mutableStateOf(TextFieldValue("")) }
 
-    Column() {
-        OutlinedTextField(
-            value=fornavn,
-            leadingIcon={Icon(imageVector= Icons.Default.Person,contentDescription=null)},
-            modifier= Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            keyboardOptions= KeyboardOptions(keyboardType= KeyboardType.Text),
-            label={Text(text="Fornavn")},
-            placeholder={Text(text="Skriv ditt etternavn")},
-            onValueChange={
-                fornavn=it
-            }
-        )
-
-        OutlinedTextField(
-            value=etternavn,
-            leadingIcon={Icon(imageVector= Icons.Default.Person,contentDescription=null)},
-            modifier= Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            keyboardOptions= KeyboardOptions(keyboardType= KeyboardType.Text),
-            label={Text(text="Etternavn")},
-            placeholder={Text(text="Skriv ditt etternavn")},
-            onValueChange={
-                etternavn=it
-            }
-        )
-
-        OutlinedTextField(
-            value=epost,
-            leadingIcon={Icon(imageVector= Icons.Default.Email,contentDescription=null)},
-            modifier= Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            label={Text(text="Epost*")},
-            placeholder={Text(text="Example")},
-            onValueChange={
-                epost=it
-            },
-            keyboardOptions= KeyboardOptions(keyboardType= KeyboardType.Password)
-        )
-
-        OutlinedTextField(
-            value=passord,
-            leadingIcon={Icon(imageVector= Icons.Default.Lock,contentDescription=null)},
-            modifier= Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            label={Text(text="Passord*")},
-            placeholder={Text(text="Example")},
-            visualTransformation= PasswordVisualTransformation(),
-            onValueChange={
-                passord=it
-            },
-            keyboardOptions= KeyboardOptions(keyboardType= KeyboardType.Password)
-        )
-
-        OutlinedTextField(
-            value=gjentaPassord,
-            leadingIcon={Icon(imageVector= Icons.Default.Lock,contentDescription=null)},
-            modifier= Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            label={Text(text="Gjenta passord*")},
-            placeholder={Text(text="Example")},
-            visualTransformation= PasswordVisualTransformation(),
-            onValueChange={
-                gjentaPassord=it
-            },
-            keyboardOptions= KeyboardOptions(keyboardType= KeyboardType.Password)
-        )
-
-        Row(
-            modifier = Modifier.padding(vertical = 24.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            TextButton(onClick = {},
-
-                ) {
-                Text("Login her.")
-            }
-            Button(
-
-                onClick = {},
+    val testColors: CardColors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.background);
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surfaceVariant)
+    ) {
+        Card(modifier = Modifier.padding(10.dp), colors = testColors) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text("Registrer bruker")
+                Text(text = "Registrer", fontSize = MaterialTheme.typography.headlineLarge.fontSize)
+                OutlinedTextField(
+                    value = fornavn,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = null,
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    label = { Text(text = "Fornavn") },
+
+                    placeholder = { Text(text = "Skriv ditt etternavn") },
+                    onValueChange = {
+                        fornavn = it
+                    }
+                )
+
+                OutlinedTextField(
+                    value = etternavn,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = null
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    label = { Text(text = "Etternavn") },
+                    placeholder = { Text(text = "Skriv ditt etternavn") },
+                    onValueChange = {
+                        etternavn = it
+                    }
+                )
+
+                OutlinedTextField(
+                    value = epost,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Email,
+                            contentDescription = null
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    label = { Text(text = "Epost*") },
+                    placeholder = { Text(text = "example@hotmail.com") },
+                    onValueChange = {
+                        epost = it
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+
+                OutlinedTextField(
+                    value = passord,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Lock,
+                            contentDescription = null
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    label = { Text(text = "Passord*") },
+                    placeholder = { Text(text = "Example") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    onValueChange = {
+                        passord = it
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+
+                OutlinedTextField(
+                    value = gjentaPassord,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Lock,
+                            contentDescription = null
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    label = { Text(text = "Gjenta passord*") },
+                    placeholder = { Text(text = "Example") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    onValueChange = {
+                        gjentaPassord = it
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    TextButton(
+                        onClick = {
+                            navController.navigate("login_screen")
+                        }) {
+                        Text("Har bruker allerede? Login her.")
+                    }
+                    Button(
+
+                        onClick = {},
+                    ) {
+                        Text("Registrer bruker")
+                    }
+                }
             }
         }
     }
