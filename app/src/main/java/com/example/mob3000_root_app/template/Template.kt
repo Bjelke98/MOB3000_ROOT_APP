18f01cd753be10685df583c5d80bbb995bb919c4
Krister Iversen <krister.iversen@outlook.com>
2022-10-04 15:21:30 +0200

    Login screen edit

--- app/src/main/java/com/example/mob3000_root_app/template/Template.kt
+++ app/src/main/java/com/example/mob3000_root_app/template/Template.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.mob3000_root_app.R
 import com.example.mob3000_root_app.components.navigation.AppNavHost
 import com.example.mob3000_root_app.components.navigation.Screen
@@ -43,27 +44,28 @@ fun Template(
             ModalDrawerSheet(
                 modifier = Modifier.width(300.dp),
             ){
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
                 Column {
                     //Title
                     Text(text = stringResource(id = R.string.company_name), modifier = Modifier.padding(16.dp), fontSize = MaterialTheme.typography.headlineMedium.fontSize)
                     //content
                     NavigationDrawerItem(
                         label = { Text(text = stringResource(id = R.string.nav_label_home)) },
-                        selected = true,
+                        selected = navBackStackEntry?.destination?.route==Screen.Home.route,
                         onClick = {
                             navigateUpTo(navController, Screen.Home)
                             closeDrawer()
                         })
                     NavigationDrawerItem(
                         label = { Text(text = stringResource(id = R.string.nav_label_home)+"2") },
-                        selected = true,
+                        selected = navBackStackEntry?.destination?.route==Screen.Home2.route,
                         onClick = {
                             navigateUpTo(navController, Screen.Home2)
                             closeDrawer()
                         })
                     NavigationDrawerItem(
                         label = { Text(text = "Articles") },
-                        selected = false,
+                        selected = navBackStackEntry?.destination?.route==Screen.Articles.route,
                         onClick = {
                             navigateUpTo(navController, Screen.Articles)
                             closeDrawer()
