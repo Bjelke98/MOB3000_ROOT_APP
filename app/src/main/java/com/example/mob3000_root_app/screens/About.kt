94b8b35fce64a579e18ad324df4574271ad258d0
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2022-11-03 01:39:12 +0100

    Edited styling for AboutUs and added dataClass for image

--- app/src/main/java/com/example/mob3000_root_app/screens/About.kt
+++ app/src/main/java/com/example/mob3000_root_app/screens/About.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.mob3000_root_app.R
 import com.example.mob3000_root_app.components.cards.AboutCard
 import com.example.mob3000_root_app.components.cards.AboutFAQCARD
 import com.example.mob3000_root_app.data.AboutData
@@ -15,12 +16,12 @@ import com.example.mob3000_root_app.data.AboutFAQ
 @Composable
 fun About() {
     val aboutList = listOf(
-        AboutData("Adrian", "Leader", "Han er flink"),
-        AboutData("Henrik", "Leader", "Han liker Kroa"),
-        AboutData("Olav", "Nestleder", "Hun liker Java"),
-        AboutData("John Ivar", "Sjef", "Hun liker Java"),
-        AboutData("Krister", "2x times", "Hun liker Java"),
-        AboutData("Herman", "Høy kar", "Hun liker Java"),
+        AboutData("Adrian Dahl", "Administrende direktør", "Han liker bananer men spiser ikke bananer men har bananer i kjøleskapet", R.drawable.rektor),
+        AboutData("Henrik Lindam", "Direktør service", "Kroa er best", R.drawable.opinion_wrong),
+        AboutData("Olav Pålerud", "Direktør forretningsstøtte", "Jeg er Olav, det er pog fordi jeg har navnet Olav i navnet mitt", R.drawable.sauce),
+        AboutData("John Ivar Lilleørene", "Viserektor forskining", "Sleeper resident", R.drawable.testing),
+        AboutData("Krister Iversen", "2x times Champion", "Bilister Trister Minister Klister Brødrister Frister Rister Angrefrister Akseptfrister Bilregister Assister Antimilitarister Annenstatminister", R.drawable.wow),
+        AboutData("Herman Simonsen", "Meget høy kar", "Litt plagsomt at Krister er 2x Champion", R.drawable.herman),
         )
 
     val aboutFaqList = listOf(
