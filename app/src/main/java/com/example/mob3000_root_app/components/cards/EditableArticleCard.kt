a2d874289d8c4ca95d8dad69670450ff080bfdfc
Linrik <henrik-lindam@hotmail.com>
2022-11-10 13:16:31 +0100

    flytte editables til cards

--- app/src/main/java/com/example/mob3000_root_app/components/cards/EditableArticleCard.kt
+++ app/src/main/java/com/example/mob3000_root_app/components/cards/EditableArticleCard.kt
@@ -1,8 +1,7 @@
-package com.example.mob3000_root_app.components.editables
+package com.example.mob3000_root_app.components.cards
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -13,14 +12,14 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.mob3000_root_app.R
-import com.example.mob3000_root_app.data.apiResponse.EventData
+import com.example.mob3000_root_app.data.apiResponse.ArticleData
+
+
 
 @Composable
-fun EditableEvent(
-    eventData : EventData
+fun EditableArticle(
+    articleData : ArticleData
 ){
-    val defaultImage = eventData.image ?: "defaultEvent.jpg"
-
     Card() {
         Row(
             Modifier
@@ -32,7 +31,7 @@ fun EditableEvent(
             Box(Modifier.sizeIn(0.dp,0.dp,100.dp,100.dp)) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
-                        .data("https://linrik.herokuapp.com/api/resources/$defaultImage")
+                        .data("https://linrik.herokuapp.com/api/resources/" + articleData.image)
                         .crossfade(true)
                         .build(),
                     placeholder = painterResource(R.drawable.testing),
@@ -44,10 +43,21 @@ fun EditableEvent(
             }
 
             Text(
-                text  = eventData.title,
+                text  = articleData.title,
                 Modifier.widthIn(0.dp,200.dp).padding(10.dp) ,
                 overflow = TextOverflow.Ellipsis,
             )
+
+            Column(verticalArrangement = Arrangement.spacedBy(15.dp)) {
+                OutlinedButton( onClick = { /*TODO*/ }, Modifier.size(100.dp,40.dp)) {
+                    Text(text  = "Delete")
+                }
+
+                OutlinedButton(onClick = { /*TODO*/ }, Modifier.size(100.dp,40.dp)) {
+                    Text(text  = "Edit")
+                }
+            }
+
         }
     }
 }
